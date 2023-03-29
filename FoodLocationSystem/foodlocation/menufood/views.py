82fd8945ed4c5158a2c6b70c499dd29351fdf1de
nttn <1951052129ngan@ou.edu.vn>
2023-03-29 11:58:37 +0700

    update func get_store get_menuitem foodDetails

--- FoodLocationSystem/foodlocation/menufood/views.py
+++ FoodLocationSystem/foodlocation/menufood/views.py
@@ -1,13 +1,14 @@
 from rest_framework import viewsets, permissions, generics, parsers, status
 from rest_framework.decorators import action
 from rest_framework.views import Response
-from .models import Food, User
-from .serializers import FoodSerializer, UserSerializer
+from .models import Food, User, MenuItem
+from .serializers import FoodSerializer, UserSerializer, StoreSerializer, MenuItemSerializer, FoodDetailsSerializer
+from .paginators import StorePaginator
 
 
-class FoodViewSet(viewsets.ModelViewSet):
+class FoodViewSet(viewsets.ViewSet, generics.RetrieveAPIView, generics.ListAPIView):
     queryset = Food.objects.filter(active=True)
-    serializer_class = FoodSerializer
+    serializer_class = FoodDetailsSerializer
 
 
 class UserViewSet(viewsets.ViewSet, generics.CreateAPIView):
@@ -31,3 +32,46 @@ class UserViewSet(viewsets.ViewSet, generics.CreateAPIView):
             u.save()
 
         return Response(UserSerializer(u, context={'request': request}).data, status=status.HTTP_200_OK)
+
+
+class StoreViewSet(viewsets.ViewSet, generics.ListAPIView):
+    queryset = User.objects.filter(is_active=True, is_verify=True)
+    serializer_class = StoreSerializer
+    pagination_class = StorePaginator
+
+    def get_queryset(self):
+        q = self.queryset
+
+        kw = self.request.query_params.get('kw')
+        if kw:
+            q = q.filter(name_store__icontains=kw)
+
+        return q
+
+    @action(methods=['get'], detail=True, url_path='menu-item')
+    def get_menu_item(self, request, pk):
+        store = self.get_object()
+        menu_item = store.menuitem_store.filter(active=True)
+
+        kw = request.query_params.get('kw')
+        if kw:
+            menu_item = menu_item.filter(name__icontains=kw)
+
+        return Response(MenuItemSerializer(menu_item, many=True).data, status=status.HTTP_200_OK)
+
+
+class MenuItemViewSet(viewsets.ViewSet, generics.ListAPIView):
+    queryset = MenuItem.objects.filter(active=True)
+    serializer_class = MenuItemSerializer
+
+    @action(methods=['get'], detail=True, url_path='foods')
+    def get_list_foods(self, request, pk):
+        menu = self.get_object()
+        food = menu.menuitem_food.filter(active=True)
+
+        kw = request.query_params.get('kw')
+        if kw:
+            food = food.filter(name__icontains=kw)
+
+        return Response(FoodSerializer(food, many=True, context={'request': request}).data, status=status.HTTP_200_OK)
+
