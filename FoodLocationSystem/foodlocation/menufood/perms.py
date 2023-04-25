9d59697afd2500d1e065553bfd440149eb129352
nttn <1951052129ngan@ou.edu.vn>
2023-04-25 21:28:57 +0700

    update nttn2

--- FoodLocationSystem/foodlocation/menufood/perms.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import permissions
-
-
-class OrderOwner(permissions.IsAuthenticated):
-    def has_object_permission(self, request, view, order):
-        return request.user and request.user == order.user
